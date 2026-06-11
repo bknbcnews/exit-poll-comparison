e690ab91b6216ded42795281b8e02f6bbef62fd9
bknbcnews <benjamin.kamisar@nbcuni.com>
2026-06-11 10:34:17 -0400

    Delete qa.js

--- qa.js
+++
@@ -1,5 +0,0 @@
-# Current live data
-
-Google Apps Script should eventually write normalized current-election JSON files here.
-
-These files are allowed to update frequently and should be clearly marked as `live_unverified`, `draft`, or `final` in metadata.
